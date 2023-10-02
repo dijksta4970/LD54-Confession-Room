c745db9237229d137a4a87eef13c7eed7feac7ee
Zhiqi Guan <aosuydchxbsk@gmail.com>
2023-10-02 14:20:44 -0700

    debug,repack

--- scenes/UI/Conversation_start.gd
+++ scenes/UI/Conversation_start.gd
@@ -5,3 +5,13 @@ extends Control
 func _ready():
 	lable.show()
 	
+	
+	
+
+
+func _on_tap_here_to_start_mouse_exited():
+	lable.hide()
+
+
+func _on_tap_here_to_start_mouse_entered():
+	lable.show()
